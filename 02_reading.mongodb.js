3b277a37ceae6e3885b77bd30afd73c2a6073001
Yashraj_K <yashrajkamble7727@gmail.com>
2026-02-13 18:16:49 +0530

    Projection In MongoDB

--- 02_reading.mongodb.js
+++ 02_reading.mongodb.js
@@ -1,5 +1,20 @@
 use("eCommerce")
 
 // db.products.find()
-db.
-db.products.find({"category": "Electronics",})
+
+// db.products.find({"name":"Wireless Mouse"})
+
+// db.products.find({"category": "Electronics",})
+
+// db.products.find({ price: { $gt: 1000 } })
+
+// db.products.find({ price: { $gte: 1000, $lte: 50000 } })
+
+// db.products.find({ $or: [{ category: "Electronics" }, { stock: { $lt: 50 } }] })
+
+// db.products.find({}, { name: 1, price: 1, _id: 0 })
+
+db.products.find().sort({ price: -1 }).skip(1).limit(3)
+
+
+
